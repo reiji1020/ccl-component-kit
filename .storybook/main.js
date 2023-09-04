90c85bf638b6fb4def60fcc5271cb9f783739fdd
reiji1020 <sk@reiji1020.info>
2023-09-05 01:32:16 +0900

    [update]storybookアップデートに伴う設定値のマイグレーション

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,8 +1,13 @@
 module.exports = {
   "stories": ["../src/**/*.stories.mdx", "../src/**/**/*.stories.@(ts|tsx)"],
   "addons": ["@storybook/addon-essentials", '@storybook/addon-interactions'],
+
   "framework": {
     name: "@storybook/react-webpack5",
     options: {}
+  },
+
+  docs: {
+    autodocs: true
   }
 };
